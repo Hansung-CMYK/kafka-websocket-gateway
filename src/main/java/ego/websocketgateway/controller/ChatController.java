28160bc6e96f84c12b31fd440fbb94db73504093
6-keem <6ukeem@gmail.com>
2025-05-06 03:11:19 +0900

    fix: send heart-beat to keep connection

--- src/main/java/ego/websocketgateway/controller/ChatController.java
+++ src/main/java/ego/websocketgateway/controller/ChatController.java
@@ -21,8 +21,7 @@ public class ChatController {
 
 	// 클라이언트 -> 서버 : /app/chat.send
 	@MessageMapping("/chat.send")
-	public void sendMessage(@Payload ChatMessage msg, Principal principal) {
-		msg.setFrom(principal.getName());
+	public void sendMessage(@Payload ChatMessage msg) {
 		kafkaTemplate.send("chat-requests", msg.getFrom(), msg);
 	}
 }
