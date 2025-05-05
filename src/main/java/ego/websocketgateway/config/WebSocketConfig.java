2e2f419798bdc975df49a2c081a395c369d758f5
6-keem <6ukeem@gmail.com>
2025-05-06 04:33:03 +0900

    fix: add bean

--- src/main/java/ego/websocketgateway/config/WebSocketConfig.java
+++ src/main/java/ego/websocketgateway/config/WebSocketConfig.java
@@ -1,7 +1,10 @@
+// WebSocketConfig.java
 package ego.websocketgateway.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -9,20 +12,32 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
-	@Override
-	public void configureMessageBroker(MessageBrokerRegistry reg) {
-		reg.enableSimpleBroker("/topic", "/queue")
-			.setHeartbeatValue(new long[]{10000, 10000});
-		reg.setApplicationDestinationPrefixes("/app");
-		reg.setUserDestinationPrefix("/user");
 
-	}
+    @Bean
+    public ThreadPoolTaskScheduler brokerHeartbeatScheduler() {
+        ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
+        scheduler.setPoolSize(1);
+        scheduler.setThreadNamePrefix("wss-heartbeat-");
+        return scheduler;
+    }
 
-	@Override
-	public void registerStompEndpoints(StompEndpointRegistry reg) {
-		reg.addEndpoint("/ws")
-			.setAllowedOriginPatterns("*")
-			.withSockJS()
-			.setHeartbeatTime(10000);
-	}
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        // application prefix
+        registry.setApplicationDestinationPrefixes("/app");
+        registry.setUserDestinationPrefix("/user");
+
+        registry.enableSimpleBroker("/topic", "/queue")
+                .setTaskScheduler(brokerHeartbeatScheduler())
+                .setHeartbeatValue(new long[]{10_000, 10_000});
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws")
+                .setAllowedOriginPatterns("*")
+                .withSockJS()
+                .setHeartbeatTime(10_000);
+    }
 }
+
