4dd83ee2c118d8872c5eccd43f549e805ae378a2
6-keem <6ukeem@gmail.com>
2025-05-20 03:51:49 +0900

    fix: remove .withSockJS() syntax for mobile connect

--- src/main/java/ego/websocketgateway/config/WebSocketConfig.java
+++ src/main/java/ego/websocketgateway/config/WebSocketConfig.java
@@ -34,8 +34,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOriginPatterns("*")
-            .withSockJS();
+                .setAllowedOriginPatterns("*");
     }
 }
 
